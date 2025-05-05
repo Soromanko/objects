ae0fba2b8d5a104636f4a18ac2904cd0c3240852
Soromanko <samuel.soroman@email.cz>
2025-05-05 15:35:17 +0200

    bank ATM progress

--- objects.py
+++ objects.py
@@ -38,6 +38,98 @@ class BankAccount:
         self.__balance += amount
         print(f"({other_account.account_holder} > {self.account_holder})Transfer accepted, New balance: {self.__balance}")
 
+class ATM:
+    def __init__(self):
+        self.pin = ""
+        self.balance = 0
+
+    def menu(self):
+        user_input = int(input("""
+            0. Enter 0 to exit
+            1. Enter 1 to create pin
+            2. Enter 2 to deposit
+            3. Enter 3 to withdraw
+            4. Enter 4 to check balance
+        """))
+
+        if user_input == 1:
+            self.create_pin()
+        elif user_input == 2:
+            self.deposit()
+        elif user_input == 3:
+            self.withdraw()
+        elif user_input == 4:
+            self.check_balance()
+        elif user_input < 0 or user_input > 4:
+            print("Please Enter a valid input")
+
+        return user_input
+
+    def create_pin(self):
+        if self.pin == "":
+            self.pin = input("Create you pin please: ")
+            print("Your pin has been set successfully")
+        else:
+            print("There is already a pin")
+
+        self.menu()
+
+    def check_pin(self):
+        pin = input("Enter your pin please: ")
+        if pin == self.pin:
+            return 1
+        else:
+            return 0
+
+    def deposit(self):
+        if self.pin == "":
+            self.create_pin()
+
+        if self.check_pin():
+            amount = input("Please enter your deposit amount: ")
+            self.balance += int(amount)
+            print("Amount has been deposited successfully")
+        else:
+            print("You have entered wrong pin, please re-enter your pin")
+            self.deposit()
+
+        self.menu()
+
+    def withdraw(self):
+        if self.pin == "":
+            self.create_pin()
+        if self.check_pin():
+            amount = int(input("Please enter your withdrawal amount or press 0 to return to menu: "))
+            if amount == 0:
+                pass
+            elif amount < 0:
+                print("Invalid amount")
+            elif self.balance >= amount:
+                self.balance -= amount
+                print("Amount has been withdrawn successfully")
+            else:
+                print("Insufficient balance")
+                self.withdraw()
+        else:
+            print("You have entered wrong pin")
+            self.withdraw()
+
+        self.menu()
+
+    def check_balance(self):
+        if self.pin == "":
+            self.create_pin()
+        if self.check_pin():
+            print(f"Your current balance is : {self.balance}")
+        else:
+            print("You have entered wrong pin")
+            self.check_balance()
+
+        self.menu()
+
+atm = ATM()
+atm.menu()
+
 #
 #
 #
@@ -53,6 +145,7 @@ def menu():
     print("3 - převod financí")
     print("4 - výběr financí")
     print("5 - vklad financí")
+    print("6 - výběr automatů")
 
     choice = int(input("Zvolte akci:"))
     if choice == 1:
@@ -62,11 +155,11 @@ def menu():
     elif choice == 3:
         transfer()
     elif choice == 4:
-        # TODO: výběr
-        print("")
+        withdraw()
     elif choice == 5:
-        # TODO: vklad
-        print("")
+        deposit()
+    elif choice == 6:
+        main()
     else:
         print("Chybná akce, vyber znovu")
 
@@ -90,6 +183,37 @@ def transfer():
     index_to = input("Účet do kterého:")
     amount = input("Částka:")
 
+
     bank_accounts[int(index_to)-1].transfer(bank_accounts[int(index_from)-1], int(amount))
+
+def withdraw():
+    print("Dostupné účty:")
+    print_accounts()
+
+    index_from = input("Účet ze kterého:")
+    amount = input("Částka:")
+
+
+    bank_accounts[int(index_from)-1].withdraw(int(amount))
+
+def deposit():
+    print("Dostupné účty:")
+    print_accounts()
+
+    index_from = input("Účet do kterého:")
+    amount = input("Částka:")
+
+
+    bank_accounts[int(index_from)-1].deposit(int(amount))
+
+def main():
+    bank = ATM()
+
+    print("Zvolte 1 pro ATM")
+
+    choice = int(input("Zvolte akci:"))
+    if choice == 1:
+        bank.menu()
+        return
 
 menu()
